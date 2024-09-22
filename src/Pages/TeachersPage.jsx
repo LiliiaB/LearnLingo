0875c3cfd91822d309f78d9e0e0bb25a500c9bf4
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-22 15:52:31 +0200

    homepage style done

--- src/Pages/TeachersPage.jsx
+++ src/Pages/TeachersPage.jsx
@@ -1,11 +1,5 @@
-import Card from "../components/Card/Card";
 import css from "./TeachersPage.module.css";
 
 export default function TeachersPage() {
-  return (
-    <div className={css.gallery}>
-      <h2>VINCITORI 2024</h2>
-      <Card />
-    </div>
-  );
+  return <div className={css.gallery}></div>;
 }
--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -4,22 +4,41 @@ import hero from "../../assets/hero.png";
 export const SectionHero = () => {
   return (
     <div className={css.hero}>
-      <div>
-        <h1 className={css.herotitle}>
-          Unlock your potential with the best{" "}
-          <span className={css.herospan}>language</span> tutors
-        </h1>
-        <p className={css.herotext}>
-          Embark on an Exciting Language Journey with Expert Language Tutors:
-          Elevate your language proficiency to new heights by connecting with
-          highly qualified and experienced tutors.
-        </p>
-        <button className={css.herobtn}>Get started</button>
+      <div className={css.heromain}>
+        <div className={css.section1}>
+          <h1 className={css.herotitle}>
+            Unlock your potential with the best{" "}
+            <span className={css.herospan}>language</span> tutors
+          </h1>
+          <p className={css.herotext}>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+          <button className={css.herobtn}>Get started</button>
+        </div>
+        <div className={css.heroimage}>
+          <img src={hero} alt="woman with macbook" />
+        </div>
       </div>
-      <div className={css.heroimage}>
-        <img src={hero} alt="woman with macbook" />
+      <div className={css.underherosection}>
+        <div className={css.stat}>
+          <h2>32,000 +</h2>
+          <p>Experienced Tutors</p>
+        </div>
+        <div className={css.stat}>
+          <h2>300,000 +</h2>
+          <p>5-Star Tutor Reviews</p>
+        </div>
+        <div className={css.stat}>
+          <h2>120 +</h2>
+          <p>Subjects Taught</p>
+        </div>
+        <div className={css.stat}>
+          <h2>200 +</h2>
+          <p>Tutor Nationalities</p>
+        </div>
       </div>
-      <div className={css.underherosection}></div>
     </div>
   );
 };
