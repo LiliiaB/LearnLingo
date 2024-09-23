9d086755e4091374a1350c117e74a780f3de9803
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-23 21:53:27 +0200

    added path

--- src/redux/teachers/operations.js
+++ src/redux/teachers/operations.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = "https://66bbb9b56a4ab5edd6393ff8.mockapi.io/adverts";
+axios.defaults.baseURL =
+  "https://learnlingo-852a3-default-rtdb.europe-west1.firebasedatabase.app";
 
 export const fetchTeachers = createAsyncThunk(
   "teachers/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/");
-      return response.data;
+      const response = await axios.get("/teachers.json");
+
+      console.log(response);
     } catch (error) {
       toast.error(error.message);
       return thunkAPI.rejectWithValue(error.message);
