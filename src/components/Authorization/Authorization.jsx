b715dfdbdbff092b11580cfbffd0c83c70b373c1
Lilia Levchenko <liliya.benatti@gmail.com>
2024-10-13 20:58:55 +0200

    working in favorites

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -3,7 +3,7 @@ import login from "../../assets/login123.png";
 
 export const Authorization = () => {
   return (
-    <div>
+    <div className={css.div}>
       <button className={css.btnlogin}>
         <img src={login} alt="Login Icon" className={css.iconlogin} />
         Log in
