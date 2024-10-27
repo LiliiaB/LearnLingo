8ed1d716d4162bf9d0f8da1942f890402144276b
Lilia Levchenko <liliya.benatti@gmail.com>
2024-10-27 11:35:04 +0100

    removed persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
 import { teachersReducer } from "./teachers/slice";
 import authReducer from "./auth/authReducer";
 
-const authPersistConfig = {
-  key: "auth",
-  storage,
-  whitelist: ["token"],
-};
-
-const teachersPersistConfig = {
-  key: "teachers",
-  storage,
-  whitelist: ["items", "favorites"],
-};
-
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authReducer),
-    teachers: persistReducer(teachersPersistConfig, teachersReducer),
+    auth: authReducer,
+    teachers: teachersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      serializableCheck: false,
     }),
   devTools: process.env.NODE_ENV === "development",
 });
-
-export const persistor = persistStore(store);
